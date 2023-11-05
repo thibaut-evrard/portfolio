708168aaf17e5b223d7297c343d7c3f4d296ab5c
thibaut evrard <thibaut.evrard.1509@gmail.com>
2023-11-05 12:45:11 +0000

    updating old od image

--- portfolio-front/src/components/Carousel/Video/Video.styles.ts
+++ portfolio-front/src/components/Carousel/Video/Video.styles.ts
@@ -1,7 +1,14 @@
+import {MOBILE_BREAKPOINT} from '@/constants/devices';
 import {motion} from 'framer-motion';
 import styled from 'styled-components';
 
 export const VideoElement = styled(motion.video)({
     display: 'block',
     objectFit: 'contain',
+    height: '100%',
+    width: '100%',
+
+    [MOBILE_BREAKPOINT]: {
+        // objectFit: 'cover',
+    },
 });
