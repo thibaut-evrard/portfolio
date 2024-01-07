9090c5623e77037bb5a3cff44e2fed76cd04f14a
thibaut evrard <thibaut.evrard.1509@gmail.com>
2024-01-07 23:54:42 +0000

    better ssr

--- portfolio-front/src/components/pages/Experiments/Gummy/Scene/Scene.tsx
+++ portfolio-front/src/components/pages/Experiments/Gummy/Scene/Scene.tsx
@@ -1,56 +1,61 @@
-import { MeshCandleMaterial } from '@/components/wgl/materials/MeshCandleMaterial/MeshCandleMaterial';
+import {MeshCandleMaterial} from '@/components/wgl/materials/MeshCandleMaterial/MeshCandleMaterial';
 import {
-  BufferGeometry,
-  Color,
-  MeshStandardMaterial,
-  PointLight,
-  PointLightHelper,
-  Texture
+    BufferGeometry,
+    Color,
+    MeshStandardMaterial,
+    PointLight,
+    PointLightHelper,
+    Texture,
 } from 'three';
 import {
-  Environment,
-  OrbitControls,
-  useGLTF,
-  useHelper,
-  useTexture
+    Environment,
+    OrbitControls,
+    useGLTF,
+    useHelper,
+    useTexture,
 } from '@react-three/drei';
-import { useRef } from 'react';
+import {useRef} from 'react';
 
 const Scene = () => {
-  const { scene } = useGLTF('/assets/3d/metaball.glb');
-  const { geometry, material } = scene.children[0] as unknown as {
-    geometry: BufferGeometry;
-    material: MeshStandardMaterial;
-  };
+    const {scene} = useGLTF('/assets/3d/metaball.glb');
+    const {geometry, material} = scene.children[0] as unknown as {
+        geometry: BufferGeometry;
+        material: MeshStandardMaterial;
+    };
 
-  const light = useRef({} as PointLight);
-  useHelper(light, PointLightHelper, 1, 'cyan');
+    const light = useRef({} as PointLight);
+    useHelper(light, PointLightHelper, 1, 'cyan');
 
-  const texture = useTexture('/assets/textures/grain-bump.jpeg');
+    const texture = useTexture('/assets/textures/grain-bump.jpeg');
 
-  return (
-    <>
-      <OrbitControls />
-      <ambientLight intensity={1} />
-      <pointLight position={[2, 0, 1]} intensity={100} distance={10} ref={light} />
-      {/* <pointLight position={[4, 0, 1]} intensity={10} distance={10} />
+    return (
+        <>
+            <OrbitControls />
+            {/* <ambientLight intensity={1} /> */}
+            <pointLight
+                position={[4, 0, 1]}
+                intensity={10}
+                decay={1}
+                ref={light}
+            />
+            {/* <pointLight position={[4, 0, 1]} intensity={10} distance={10} />
       <pointLight position={[1, 1, 5]} intensity={10} distance={10} /> */}
-      <Environment
-        files={'assets/textures/syferfontein_0d_clear_puresky_1k.hdr'}
-        background
-      />
-      <mesh args={[geometry, undefined]}>
-        <MeshCandleMaterial
-          gummyMap={material.emissiveMap as Texture}
-          bumpMap={texture}
-          bumpScale={0.01}
-          coreColor={new Color('#175700')}
-          color={new Color('#f0863a')}
-          roughness={0.5}
-        />
-      </mesh>
-    </>
-  );
+            <Environment
+                files={'assets/textures/syferfontein_0d_clear_puresky_1k.hdr'}
+                background
+            />
+            <mesh args={[geometry, undefined]}>
+                <MeshCandleMaterial
+                    gummyMap={material.emissiveMap as Texture}
+                    bumpMap={texture}
+                    bumpScale={0.01}
+                    coreColor={new Color('#fa0000')}
+                    color={new Color('#f0863a')}
+                    roughness={0.2}
+                />
+            </mesh>
+        </>
+    );
 };
 
 export default Scene;
--- portfolio-front/src/components/wgl/materials/MeshCandleMaterial/shader/fragment.ts
+++ portfolio-front/src/components/wgl/materials/MeshCandleMaterial/shader/fragment.ts
@@ -1,211 +1,3 @@
-// const fragment = `
-// #define STANDARD
-
-// #ifdef PHYSICAL
-// 	#define IOR
-// 	#define SPECULAR
-// #endif
-
-// uniform vec3 diffuse;
-// uniform vec3 emissive;
-// uniform float roughness;
-// uniform float metalness;
-// uniform float opacity;
-
-// #ifdef IOR
-// 	uniform float ior;
-// #endif
-
-// #ifdef SPECULAR
-// 	uniform float specularIntensity;
-// 	uniform vec3 specularColor;
-
-// 	#ifdef USE_SPECULARINTENSITYMAP
-// 		uniform sampler2D specularIntensityMap;
-// 	#endif
-
-// 	#ifdef USE_SPECULARCOLORMAP
-// 		uniform sampler2D specularColorMap;
-// 	#endif
-// #endif
-
-// #ifdef USE_CLEARCOAT
-// 	uniform float clearcoat;
-// 	uniform float clearcoatRoughness;
-// #endif
-
-// #ifdef USE_IRIDESCENCE
-// 	uniform float iridescence;
-// 	uniform float iridescenceIOR;
-// 	uniform float iridescenceThicknessMinimum;
-// 	uniform float iridescenceThicknessMaximum;
-// #endif
-
-// #ifdef USE_SHEEN
-// 	uniform vec3 sheenColor;
-// 	uniform float sheenRoughness;
-
-// 	#ifdef USE_SHEENCOLORMAP
-// 		uniform sampler2D sheenColorMap;
-// 	#endif
-
-// 	#ifdef USE_SHEENROUGHNESSMAP
-// 		uniform sampler2D sheenRoughnessMap;
-// 	#endif
-// #endif
-
-// varying vec3 vViewPosition;
-
-// #include <common>
-// #include <packing>
-// #include <dithering_pars_fragment>
-// #include <color_pars_fragment>
-// #include <uv_pars_fragment>
-// #include <uv2_pars_fragment>
-// #include <map_pars_fragment>
-// #include <alphamap_pars_fragment>
-// #include <alphatest_pars_fragment>
-// #include <aomap_pars_fragment>
-// #include <lightmap_pars_fragment>
-// #include <emissivemap_pars_fragment>
-// #include <bsdfs>
-// #include <iridescence_fragment>
-// #include <cube_uv_reflection_fragment>
-// #include <envmap_common_pars_fragment>
-// #include <envmap_physical_pars_fragment>
-// #include <fog_pars_fragment>
-// #include <lights_pars_begin>
-// #include <normal_pars_fragment>
-// #include <lights_physical_pars_fragment>
-// #include <transmission_pars_fragment>
-// #include <shadowmap_pars_fragment>
-// #include <bumpmap_pars_fragment>
-// #include <normalmap_pars_fragment>
-// #include <clearcoat_pars_fragment>
-// #include <iridescence_pars_fragment>
-// #include <roughnessmap_pars_fragment>
-// #include <metalnessmap_pars_fragment>
-// #include <logdepthbuf_pars_fragment>
-// #include <clipping_planes_pars_fragment>
-
-// // CUSTOM START
-// float getGummyFactor(vec3 normal, float thickness) {
-// 	float reverseThickness = pow(thickness, 5.);
-// 	float normalFactor = pow(abs(normal.x), 8.);
-// 	float gummy = max(normalFactor, reverseThickness);
-// 	return gummy;
-// }
-
-// vec3 getGummyEmissionFactor(GeometricContext geometry, float gummyFactor, vec3 gummyDiffuse) {
-// 	#if ( NUM_POINT_LIGHTS > 0 ) && defined( RE_Direct )
-// 	PointLight pointLight;
-// 	vec3 fragEmit = vec3(0.);
-
-// 	for ( int i = 0; i < NUM_POINT_LIGHTS; i ++ ) {
-// 		pointLight = pointLights[ i ];
-
-// 		vec3 lightToFrag = pointLight.position - geometry.position;
-// 		vec3 lightToFragDirection = normalize(lightToFrag);
-// 		float lightToFragDistance = clamp(length(lightToFrag), 0., 1.);
-// 		float lightIntensity = getDistanceAttenuation( lightToFragDistance, pointLight.distance, pointLight.decay );
-
-// 		if(lightToFragDistance < 1.) {
-// 			float intensityFactor = pow(lightIntensity, 2.);
-// 			float factor = intensityFactor;
-// 			fragEmit += pointLight.color * factor + intensityFactor * gummyFactor;
-// 		}
-// 	}
-
-// 	vec3 col = gummyDiffuse * fragEmit;
-// 	return clamp(col, 0., 1.);
-
-// 	#endif
-// }
-
-// uniform sampler2D gummyMap;
-// uniform vec3 coreColor;
-// // CUSTOM END
-
-// void main() {
-
-// 	#include <clipping_planes_fragment>
-
-// 	vec4 diffuseColor = vec4( diffuse, opacity );
-// 	ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
-// 	vec3 totalEmissiveRadiance = emissive;
-
-// 	#include <logdepthbuf_fragment>
-// 	#include <map_fragment>
-// 	#include <color_fragment>
-
-// 	#include <alphamap_fragment>
-// 	#include <alphatest_fragment>
-// 	#include <roughnessmap_fragment>
-// 	#include <metalnessmap_fragment>
-// 	#include <normal_fragment_begin>
-// 	#include <normal_fragment_maps>
-// 	#include <clearcoat_normal_fragment_begin>
-// 	#include <clearcoat_normal_fragment_maps>
-// 	#include <emissivemap_fragment>
-
-// 	// CUSTOM START
-// 	float thickness = 1. - texture2D( gummyMap, vUv ).r;
-// 	float gummyFactor = getGummyFactor(normal, thickness);
-// 	vec3 gummyDiffuse = mix(coreColor, diffuse, gummyFactor);
-// 	diffuseColor = vec4( gummyDiffuse, opacity );
-// 	// CUSTOM END
-
-// 	// accumulation
-// 	#include <lights_physical_fragment>
-// 	#include <lights_fragment_begin>
-// 	#include <lights_fragment_maps>
-// 	#include <lights_fragment_end>
-
-// 	// modulation
-// 	#include <aomap_fragment>
-
-// 	vec3 totalDiffuse = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse;
-// 	vec3 totalSpecular = reflectedLight.directSpecular + reflectedLight.indirectSpecular;
-
-// 	#include <transmission_fragment>
-
-// 	// CUSTOM START
-// 	vec3 gummyEmissionColor = getGummyEmissionFactor(geometry, gummyFactor, gummyDiffuse);
-// 	totalEmissiveRadiance = gummyEmissionColor;
-// 	// CUSTOM END
-
-// 	vec3 outgoingLight = totalDiffuse + totalSpecular + totalEmissiveRadiance;
-
-// 	#ifdef USE_SHEEN
-
-// 		// Sheen energy compensation approximation calculation can be found at the end of
-// 		// https://drive.google.com/file/d/1T0D1VSyR4AllqIJTQAraEIzjlb5h4FKH/view?usp=sharing
-// 		float sheenEnergyComp = 1.0 - 0.157 * max3( material.sheenColor );
-
-// 		outgoingLight = outgoingLight * sheenEnergyComp + sheenSpecular;
-
-// 	#endif
-
-// 	#ifdef USE_CLEARCOAT
-
-// 		float dotNVcc = saturate( dot( geometry.clearcoatNormal, geometry.viewDir ) );
-
-// 		vec3 Fcc = F_Schlick( material.clearcoatF0, material.clearcoatF90, dotNVcc );
-
-// 		outgoingLight = outgoingLight * ( 1.0 - material.clearcoat * Fcc ) + clearcoatSpecular * material.clearcoat;
-
-// 	#endif
-
-// 	#include <output_fragment>
-// 	#include <tonemapping_fragment>
-// 	#include <encodings_fragment>
-// 	#include <fog_fragment>
-// 	#include <premultiplied_alpha_fragment>
-// 	#include <dithering_fragment>
-// }
-
-// `;
-
 const fragment = `
 #define STANDARD
 
@@ -304,6 +96,45 @@ varying vec3 vViewPosition;
 #include <clipping_planes_pars_fragment>
 
 // CUSTOM START
+vec3 getGummyEmissionStrength(vec3 geometryPosition, vec3 normal, float thickness) {
+	float emit = 0.;
+
+	#if ( NUM_POINT_LIGHTS > 0 ) && defined( RE_Direct )
+	PointLight pointLight;
+
+	for ( int i = 0; i < NUM_POINT_LIGHTS; i ++ ) {
+		pointLight = pointLights[ i ];
+
+		vec3 lightToFrag = pointLight.position - geometryPosition;
+		vec3 camToLight = -normalize(pointLight.position);
+		vec3 lightToFragDirection = normalize(lightToFrag + normal);
+
+		vec3 refractedLightToFragThickness = refract(lightToFragDirection, normal, thickness);
+		float angleLightNormal = dot(lightToFragDirection, -normal);
+
+		float lightIntensity = getDistanceAttenuation( length(lightToFrag), pointLight.distance, pointLight.decay );
+		// vec3 lightDirection = normalize(- pointLight.position);
+		// vec3 geometryDirection = normalize(geometryPosition);
+		// vec3 lightToFragDirection = normalize(lightToFrag);
+		// float lightToFragDistance = length(lightToFrag);
+
+		// float lightThroughCamera = dot(lightToFragDirection, geometryDirection) / 1.;
+		// float lightThroughNormal = pow(dot(lightToFragDirection, -normal), 1.);
+		// lightThroughNormal = clamp(lightThroughNormal, 0., 1.);
+
+		// float lightIntensity = getDistanceAttenuation( lightToFragDistance, pointLight.distance, pointLight.decay );
+
+		float lightThroughIntensity = pow(max(dot(camToLight, -refractedLightToFragThickness), 0.), 2.);
+		
+		emit = lightThroughIntensity; // + lightIntensity;
+		//emit += lightThroughIntensity + lightIntensity;
+		//return lightThroughNormal * lightIntensity;
+	}
+
+	#endif
+	return vec3(emit);
+}
+
 float getGummyFactor(vec3 normal, float thickness) {
 	float reverseThickness = pow(thickness, 1.);
 	float intensity = 4.;
@@ -311,31 +142,6 @@ float getGummyFactor(vec3 normal, float thickness) {
 	float gummy = normalFactor;
 	return min(gummy, 1.);
 }
-
-// vec3 getGummyEmissionFactor(vec3 geometryPosition, float gummyFactor, vec3 gummyDiffuse, vec3 normal) {
-// 	#if ( NUM_POINT_LIGHTS > 0 ) && defined( RE_Direct )
-// 	PointLight pointLight;
-// 	vec3 fragEmit = vec3(0.);
-
-// 	for ( int i = 0; i < NUM_POINT_LIGHTS; i ++ ) {
-// 		pointLight = pointLights[ i ];
-		
-// 		vec3 lightToFrag = pointLight.position - geometryPosition;
-// 		vec3 lightToFragDirection = normalize(lightToFrag);
-
-// 		float lightThroughNormal = pow(dot(lightToFragDirection, -normal), 3.);
-// 		float lightToFragDistance = length(lightToFrag);
-// 		float lightIntensity = getDistanceAttenuation( lightToFragDistance, pointLight.distance, pointLight.decay );
-
-// 		fragEmit += vec3(lightThroughNormal) * gummyFactor * (1. / (pointLight.distance * pointLight.decay));
-// 	}
-
-// 	vec3 col = gummyDiffuse * fragEmit;
-// 	return clamp(col, 0., 1.);
-
-// 	#endif
-// 	return vec3(0.);
-// }
 
 float getGummyEmission(vec3 geometryPosition, float gummyFactor, vec3 normal) {
 	float emit = 0.;
@@ -362,6 +168,7 @@ float getGummyEmission(vec3 geometryPosition, float gummyFactor, vec3 normal) {
 
 uniform sampler2D gummyMap;
 uniform vec3 coreColor;
+uniform mat4 modelMatrix;
 // CUSTOM END
 
 void main() {
@@ -387,10 +194,7 @@ void main() {
 	#include <emissivemap_fragment>
 
 	// CUSTOM START
-	float thickness = 1. - texture2D( gummyMap, vUv ).r;
-	float gummyFactor = getGummyFactor(normal, thickness);
-	vec3 gummyDiffuse = mix(coreColor, diffuse, gummyFactor);
-	diffuseColor = vec4( gummyDiffuse, opacity );
+	//diffuseColor = vec4( diffuse, opacity );
 	// CUSTOM END
 
 	// accumulation
@@ -408,11 +212,21 @@ void main() {
 	#include <transmission_fragment>
 
 	// CUSTOM START
-	float gummyEmission = getGummyEmission(geometryPosition, gummyFactor, normal);
-	vec3 gummyEmissionColor = mix(diffuse, coreColor, gummyEmission);
-	// CUSTOM END
+	float thickness = 1. - texture2D( gummyMap, vUv ).r;
+	vec3 globalPos = (modelMatrix * vec4(geometryPosition, 1.0 )).xyz;
+	vec3 gummyEmission = getGummyEmissionStrength(geometryPosition, normal, pow(thickness, 5.));
+	float ge = gummyEmission.x;
+	vec3 col = mix(diffuse, coreColor, ge);
+	// diffuse
+	// coreColor
+	// float gummyFactor = getGummyFactor(normal, thickness);
+	// vec3 gummyDiffuse = mix(coreColor, diffuse, gummyFactor);
+	// float gummyEmission = getGummyEmission(geometryPosition, gummyFactor, normal);
+	// vec3 gummyEmissionColor = mix(diffuse, coreColor, gummyEmission);
 
-	vec3 outgoingLight = vec3(gummyEmissionColor); // + gummyDiffuse * 0.4 + totalSpecular + totalEmissiveRadiance;
+	//vec3 outgoingLight = vec3(gummyEmission);
+	vec3 outgoingLight = col + totalSpecular + totalEmissiveRadiance;
+	// CUSTOM END
 
 	#ifdef USE_SHEEN
 
