1972e7e65530c2068f6f92047b7bb15571197fcc
thibaut <you@example.com>
2024-01-14 23:02:54 +0000

    fixing pan

--- portfolio-front/src/components/Carousel/Carousel.styles.ts
+++ portfolio-front/src/components/Carousel/Carousel.styles.ts
@@ -1,60 +1,60 @@
-import {BORDER_RADIUS} from '@/constants/style';
-import {motion} from 'framer-motion';
+import { BORDER_RADIUS } from '@/constants/style';
+import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
 export const Container = styled.div({
-    position: 'relative',
+  position: 'relative',
 
-    display: 'flex',
-    flexDirection: 'column',
-    width: '100%',
-    height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  height: '100%',
 
-    marginTop: '80px',
-    marginBottom: '40px',
+  marginTop: '80px',
+  marginBottom: '40px'
 });
 
 export const MediaContainer = styled(motion.div)({
-    position: 'relative',
-    display: 'block',
+  position: 'relative',
+  display: 'block',
 
-    width: '100%',
-    height: '80vh',
-    maxHeight: '80vh',
+  width: '100%',
+  height: '80vh',
+  maxHeight: '80vh',
 
-    overflow: 'hidden',
-    borderRadius: BORDER_RADIUS,
-    backgroundColor: 'black',
+  overflow: 'hidden',
+  borderRadius: BORDER_RADIUS,
+  backgroundColor: 'black',
 
-    touchAction: 'none',
+  touchAction: 'pan-y'
 });
 
 export const MediaWrapper = styled(motion.div)({
-    position: 'relative',
+  position: 'relative',
 
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'nowrap',
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'nowrap',
 
-    height: '100%',
-    width: '100%',
+  height: '100%',
+  width: '100%'
 });
 
 export const MediaItemContainer = styled.div({
-    position: 'relative',
-    display: 'flex',
-    justifyContent: 'center',
+  position: 'relative',
+  display: 'flex',
+  justifyContent: 'center',
 
-    width: '100%',
-    minWidth: '100%',
-    img: {
-        userSelect: 'none',
-    },
+  width: '100%',
+  minWidth: '100%',
+  img: {
+    userSelect: 'none'
+  }
 });
 
 export const Image = styled(motion.img)({
-    width: '100%',
-    height: '100%',
-    objectFit: 'contain',
-    userSelect: 'none',
+  width: '100%',
+  height: '100%',
+  objectFit: 'contain',
+  userSelect: 'none'
 });
--- portfolio-front/src/components/Carousel/Carousel.tsx
+++ portfolio-front/src/components/Carousel/Carousel.tsx
@@ -1,77 +1,73 @@
-import {FC, useRef, useState} from 'react';
-import {ICarousel} from './Carousel.types';
+import { FC, useRef, useState } from 'react';
+import { ICarousel } from './Carousel.types';
 import {
-    Container,
-    Image,
-    MediaContainer,
-    MediaItemContainer,
-    MediaWrapper,
+  Container,
+  Image,
+  MediaContainer,
+  MediaItemContainer,
+  MediaWrapper
 } from './Carousel.styles';
-import {getMediaTypes} from '@/utils/getMediaTypes';
+import { getMediaTypes } from '@/utils/getMediaTypes';
 import Menu from './Menu/Menu';
-import {useCarouselControls} from '@/hooks/carousel/useCarouselControls';
+import { useCarouselControls } from '@/hooks/carousel/useCarouselControls';
 import Video from './Video/Video';
 
-const Carousel: FC<ICarousel> = ({media}) => {
-    const {videos, images} = getMediaTypes(media);
-    const mediaContainerRef = useRef<HTMLDivElement>(null);
-    const [activeMedia, setActiveMedia] = useState(0);
+const Carousel: FC<ICarousel> = ({ media }) => {
+  const { videos, images } = getMediaTypes(media);
+  const mediaContainerRef = useRef<HTMLDivElement>(null);
+  const [activeMedia, setActiveMedia] = useState(0);
 
-    const {onPan, onPanEnd, goTo, progress} = useCarouselControls(
-        mediaContainerRef,
-        media.length,
-        setActiveMedia
-    );
+  const { onPan, onPanEnd, goTo, progress } = useCarouselControls(
+    mediaContainerRef,
+    media.length,
+    setActiveMedia
+  );
 
-    const handleOnSelect = (index: number) => {
-        setActiveMedia(index);
-        goTo(index);
-    };
+  const handleOnSelect = (index: number) => {
+    setActiveMedia(index);
+    goTo(index);
+  };
 
-    return (
-        <Container>
-            <MediaContainer
+  return (
+    <Container>
+      <MediaContainer onPan={onPan} onPanEnd={onPanEnd} ref={mediaContainerRef}>
+        <MediaWrapper style={{ translateX: progress }}>
+          {videos.map((video, index) => (
+            <MediaItemContainer>
+              <Video
+                key={index}
+                media={video}
                 onPan={onPan}
                 onPanEnd={onPanEnd}
-                ref={mediaContainerRef}
-            >
-                <MediaWrapper style={{translateX: progress}}>
-                    {videos.map((video, index) => (
-                        <MediaItemContainer>
-                            <Video
-                                key={index}
-                                media={video}
-                                onPan={onPan}
-                                onPanEnd={onPanEnd}
-                                active={index === activeMedia}
-                            />
-                        </MediaItemContainer>
-                    ))}
-                    {images.map((image, index) => (
-                        <MediaItemContainer>
-                            <Image
-                                key={index}
-                                src={image.src}
-                                width={'1400'}
-                                height='800'
-                                alt='img'
-                                onPan={onPan}
-                                onPanEnd={onPanEnd}
-                                draggable={false}
-                            />
-                        </MediaItemContainer>
-                    ))}
-                </MediaWrapper>
-            </MediaContainer>
-            {media.length > 1 && (
-                <Menu
-                    mediaCount={media.length}
-                    activeIndex={activeMedia}
-                    onSelect={handleOnSelect}
-                />
-            )}
-        </Container>
-    );
+                active={index === activeMedia}
+              />
+            </MediaItemContainer>
+          ))}
+          {images.map((image, index) => (
+            <MediaItemContainer>
+              <Image
+                key={index}
+                src={image.src}
+                width={'1400'}
+                height='800'
+                alt='img'
+                onPan={onPan}
+                onPanEnd={onPanEnd}
+                draggable={false}
+              />
+            </MediaItemContainer>
+          ))}
+        </MediaWrapper>
+      </MediaContainer>
+      {media.length > 1 && (
+        <Menu
+          mediaCount={media.length}
+          activeIndex={activeMedia}
+          onSelect={handleOnSelect}
+        />
+      )}
+    </Container>
+  );
 };
 
 export default Carousel;
