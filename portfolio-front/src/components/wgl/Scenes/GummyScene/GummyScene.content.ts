8a9aaf40c872c0e4ab551b40882261a82ca30ab3
thibaut <you@example.com>
2024-01-16 10:16:06 +0000

    adding gummy page

--- portfolio-front/src/components/wgl/Scenes/GummyScene/GummyScene.content.ts
+++ portfolio-front/src/components/wgl/Scenes/GummyScene/GummyScene.content.ts
@@ -1,4 +1,4 @@
 export const content = {
-  model: 'wgl/gummyScene/gummies.glb',
-  bumpMap: 'wgl/common/textures/grain-bump.jpeg'
+    model: "/wgl/gummyScene/gummies.glb",
+    bumpMap: "/wgl/common/textures/grain-bump.jpeg",
 };
