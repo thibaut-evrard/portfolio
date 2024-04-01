bfd8eafdb3a5f583f35c4a7bf3eb211a6097f393
thibaut evrard <thibaut.evrard.1509@gmail.com>
2024-04-01 19:45:37 +0100

    fixing env

--- portfolio-front/src/components/wgl/Scenes/BubblesScene/BubblesScene.content.ts
+++ portfolio-front/src/components/wgl/Scenes/BubblesScene/BubblesScene.content.ts
@@ -1,5 +1,5 @@
 export const content = {
     background: "/wgl/bubblesScene/background.png",
-    hdri: "/wgl/common/hdr/industrial_sunset_puresky_1k.hdr",
+    hdri: "wgl/common/hdr/env.hdr",
     model: "/wgl/bubblesScene/tibo-bubbles.glb",
 };
