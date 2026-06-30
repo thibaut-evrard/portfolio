3a86248c0ee0732958c565d9a3395214d9e8effc
thibaut evrard <thibaut.evrard.1509@gmail.com>
2026-06-30 19:53:06 +0100

    Remove finecast

--- portfolio-front/src/content/Projects/projects.ts
+++ portfolio-front/src/content/Projects/projects.ts
@@ -1,13 +1,11 @@
 import type { IProjectContent, ProjectName } from './projects.types';
 import { dmsIconWall } from './Project/dms-icon-wall';
-import { finecastWebgl } from './Project/finecast-webgl';
 import { oysterDisaster } from './Project/oyster-disaster';
 import { blurryBananas } from './Project/blurry-bananas';
 import { mediapipeExperiments } from './Project/mediapipe-experiments';
 
 export const PROJECT_NAMES = [
   'dms-icon-wall',
-  'finecast-webgl',
   'oyster-disaster',
   'blurry-bananas',
   'mediapipe-experiments',
@@ -15,7 +13,6 @@ export const PROJECT_NAMES = [
 
 export const ProjectsContent: Record<ProjectName, IProjectContent> = {
   ['dms-icon-wall']: dmsIconWall,
-  ['finecast-webgl']: finecastWebgl,
   ['oyster-disaster']: oysterDisaster,
   ['blurry-bananas']: blurryBananas,
   ['mediapipe-experiments']: mediapipeExperiments,
@@ -24,7 +21,6 @@ export const ProjectsContent: Record<ProjectName, IProjectContent> = {
 export const ProjectList: IProjectContent[] = [
   ProjectsContent['dms-icon-wall'],
   ProjectsContent['oyster-disaster'],
-  ProjectsContent['finecast-webgl'],
   ProjectsContent['blurry-bananas'],
   ProjectsContent['mediapipe-experiments'],
 ];
