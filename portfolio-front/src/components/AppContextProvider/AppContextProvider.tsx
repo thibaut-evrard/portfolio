0d83fce9528ca99bf96c06402a6d2af49ca55a76
thibaut evrard <thibaut.evrard.1509@gmail.com>
2026-07-12 14:00:16 +0100

    Render on first load

--- portfolio-front/src/components/AppContextProvider/AppContextProvider.tsx
+++ portfolio-front/src/components/AppContextProvider/AppContextProvider.tsx
@@ -1,17 +1,23 @@
 'use client';
 import { AppContext } from '@/context/AppContext';
-import { FC, PropsWithChildren, useState } from 'react';
+import { FC, PropsWithChildren, useEffect, useState } from 'react';
 
 const AppContextProvider: FC<PropsWithChildren> = ({ children }) => {
   const [firstLoad, setFirstLoad] = useState(true);
+  const [loaded, setLoaded] = useState(false);
 
   function completeFirstLoad() {
     setFirstLoad(false);
   }
 
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setLoaded(true);
+  }, []);
+
   return (
     <AppContext.Provider value={{ firstLoad, completeFirstLoad }}>
-      {children}
+      {loaded && children}
     </AppContext.Provider>
   );
 };
